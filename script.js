b8abe1723bd065d152510c758ad3304f1377aef1
adamvaughn <adamdvaughn@gmail.com>
2022-01-09 22:17:02 -0600

    confirmations connected to arrays

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var generateBtn = document.querySelector("#generate");
 // Character Arrays
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "="];
 
 // Confirmations
@@ -38,6 +38,30 @@ function generatePassword() {
         var confirmNumber = confirm("Include numbers?");
         var confirmSpecChar = confirm("Include special characters?")    
     } 
+  
+    var passwordCharacters = []
+    
+    //Confirmations connected to arrays
+
+    if (confirmLower) {
+      passwordCharacters = passwordCharacters.concat(lowercase)
+    }
+
+    if (confirmUpper) {
+      passwordCharacters = passwordCharacters.concat(uppercase)
+    }
+
+    if (confirmNumber) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+
+    if (confirmSpecChar) {
+      passwordCharacters = passwordCharacters.concat(specChar)
+    }
+
+      console.log(passwordCharacters)
+}
+
 
 // Write password to the #password input
 function writePassword() {
